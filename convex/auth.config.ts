4555ca31a91b07eaad3155ddb68d6379779e668c
João Vitor Ceolin Prudente <jvitor.prudente@gmail.com>
2025-05-14 18:11:08 -0300

    fix: auth config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,7 +2,7 @@ const authConfig = {
   providers: [
     {
       applicationID: "convex",
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
+      domain: process.env.VITE_CLERK_FRONTEND_API_URL,
     }
   ],
 };
